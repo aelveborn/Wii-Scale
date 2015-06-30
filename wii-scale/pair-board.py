123755e12c47bc649186f27dc8f2b4a436d7be9e
Andreas Älveborn <aelveborn@gmail.com>
2015-06-30 22:43:24 +0200

    Added pairing functions

---
+++ wii-scale/pair-board.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Author: Andreas Älveborn
+# URL: https://github.com/aelveborn/Wii-Scale
+#
+# This file is part of Wii-Scale
+#
+# ----------------------------------------------------------------------------
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2015 Andreas Älveborn
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import wiiboard
+import subprocess
+
+from bluetooth import *
+
+def main():
+	address = None
+	board = wiiboard.Wiiboard()
+	
+	print "Press the red sync button under the battery hatch"
+
+	while(address is None):
+		print "Searching for Wii Balance Board..."
+		address = board.discover()
+
+	cmd_wiibind = "../scripts/xwiibind.sh " + address
+	cmd_npm = "npm config set wii-scale:address " + address
+	
+	subprocess.call(cmd_wiibind, shell=True)
+	
+	print "Finishing up.."
+
+	subprocess.check_call(cmd_npm, shell=True)
+
+	print "All done!"
+
+
+if __name__ == "__main__":
+	main()
