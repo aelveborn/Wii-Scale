6c0dea62c56eab118c72812ebdab462d48a2b8de
Matt Robinson <git@nerdoftheherd.com>
2016-09-04 21:36:04 +0100

    First hack of moving backend from Python to C++
    The hid_wiimote module seems to interfere with communication between
    Python and the balance board (as I think it connects first).  Instead of
    fighting against it, rewrite the backend in C++ and use xwiimote to talk
    to the hid_wiimote module.

---
+++ wii-scale/wii-scale.cpp
@@ -0,0 +1,293 @@
+/*
+ * This file is part of Wii-Scale
+ * Copyright © 2015 Andreas Älveborn
+ * Copyright © 2016 Matt Robinson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+#include <sio_client.h>
+#include <xwiimote.h>
+#include <poll.h>
+
+namespace options = boost::program_options;
+
+sio::socket::ptr current_socket;
+struct xwii_iface *board = NULL;
+
+const int sensitivity = 3000; // as 10ths of a kg
+
+void send_status(std::string status)
+{
+    std::cout << "Sending status: " << status << std::endl;
+
+    auto object = sio::object_message::create();
+    std::static_pointer_cast<sio::object_message>(object)->insert("status", status);
+    current_socket->emit("wiiscale-status", object);
+}
+
+void send_weight(std::vector<uint32_t> totals)
+{
+    static std::chrono::high_resolution_clock::time_point lastTime;
+    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - lastTime);
+
+    if(ms.count() < 100)
+    {
+        // Only send the weight every 0.1 of a second
+        return;
+    }
+
+    lastTime = std::chrono::high_resolution_clock::now();
+    uint32_t total = 0;
+
+    for(auto iter = totals.begin(); iter != totals.end(); ++iter)
+    {
+        total += *iter;
+    }
+
+    total /= totals.size();
+
+    auto value = sio::double_message::create((double)total / 100);
+    auto object = sio::object_message::create();
+    std::static_pointer_cast<sio::object_message>(object)->insert("totalWeight", value);
+
+    current_socket->emit("wiiscale-weight", object);
+}
+
+void connect()
+{
+    struct xwii_monitor* monitor = xwii_monitor_new(false, false);
+
+    if(monitor == NULL)
+    {
+        std::cerr << "Failed to create monitor" << std::endl;
+        return;
+    }
+
+    char *path;
+
+    for(;;)
+    {
+        if((path = xwii_monitor_poll(monitor)) == NULL)
+        {
+            std::cerr << "Unable to find a balance board" << std::endl;
+            sleep(1);
+            return;
+        }
+
+        int ret = xwii_iface_new(&board, path);
+        free(path);
+
+        if(ret)
+        {
+            std::cerr << "Failed to connect to " << path << " ret: " << ret << std::endl;
+            continue;
+        }
+
+        if((xwii_iface_available(board) & XWII_IFACE_BALANCE_BOARD) == 0)
+        {
+            // Not a balance board
+            xwii_iface_unref(board);
+            board = NULL;
+
+            continue;
+        }
+
+        ret = xwii_iface_open(board, XWII_IFACE_BALANCE_BOARD);
+
+        if(ret)
+        {
+            std::cerr << "Cannot enable Balance Board: " << ret << std::endl;
+
+            xwii_iface_unref(board);
+            board = NULL;
+
+            return;
+        }
+    }
+}
+
+int main(int argc, const char* argv[])
+{
+    std::string host;
+    int port;
+    int calibrate;
+    std::string address;
+
+    options::options_description desc("wii-scale");
+
+    desc.add_options()
+        ("help", "Show this help")
+        ("host,h", options::value<std::string>(&host)->default_value("localhost"), "host")
+        ("port,p", options::value<int>(&port)->default_value(8080), "port")
+        ("calibrate,c", options::value<int>(&calibrate)->default_value(0), "calibration kg")
+        ("address,a", options::value<std::string>(&address), "mac-address")
+    ;
+
+    options::variables_map map;
+    options::store(options::parse_command_line(argc, argv, desc), map);
+    options::notify(map);
+
+    if (map.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    std::cout << "Wii-Scale started" << std::endl;
+
+    sio::client client;
+    client.connect("http://" + host + ":" + std::to_string(port));
+    current_socket = client.socket();
+
+    bool ready = false;
+    bool sleep = true;
+    bool connected = false;
+
+    current_socket->on("wiiscale-connect", [&](sio::event& ev)
+    {
+        std::cout << "Requested connect" << std::endl;
+        sleep = false;
+    });
+
+    current_socket->on("wiiscale-disconnect", [&](sio::event& ev)
+    {
+        std::cout << "Requested disconnect" << std::endl;
+        // TODO: Disconnect
+    });
+
+    // Scale
+    for(;;)
+    {
+        // Check if connection status changed
+        if(connected != (board != NULL))
+        {
+            connected = (board != NULL);
+
+            if(connected)
+            {
+                send_status("CONNECTED");
+            }
+            else
+            {
+                send_status("DISCONNECTED");
+            }
+        }
+
+        // Waiting for disconnect/sleep command
+        usleep(100000);
+
+        if(sleep)
+        {
+            continue;
+        }
+
+        // Reset
+        int done = false;
+        std::vector<uint32_t> total;
+        bool firstStep = true;
+        int skipReadings = 10;
+
+        // Connect to balance board
+        if(board == NULL)
+        {
+            send_status("CONNECTING");
+            connect();
+
+            if(board == NULL)
+            {
+                sleep = true;
+            }
+            else
+            {
+                connected = true;
+                send_status("CONNECTED");
+            }
+        }
+
+        // Board is connected and ready
+        if(board != NULL)
+        {
+            // Post ready status once
+            if(!ready)
+            {
+                ready = true;
+                //board.setLight(true)
+                send_status("READY");
+            }
+
+            while(!done)
+            {
+                struct xwii_event event;
+                int ret = xwii_iface_dispatch(board, &event, sizeof(event));
+
+                if(ret == -EAGAIN)
+                {
+                    continue;
+                }
+                else if(ret)
+                {
+                    std::cerr << "Error: Read failed with err: " << ret << std::endl;
+                    exit(1);
+                }
+                else if (event.type != XWII_EVENT_BALANCE_BOARD)
+                {
+                    continue;
+                }
+
+                // Measure weight
+                uint32_t totalWeight = 0;
+
+                for(int i = 0; i < 4; i++)
+                {
+                    totalWeight += event.v.abs[i].x;
+                }
+
+                if(totalWeight <= sensitivity)
+                {
+                    if(!firstStep)
+                    {
+                        done = true;
+                        send_status("DONE");
+                    }
+
+                    continue;
+                }
+
+
+                if(firstStep)
+                {
+                    firstStep = false;
+                    send_status("MEASURING");
+                }
+
+                // Skips the first readings when the user steps on the balance board
+                skipReadings -= 1;
+
+                if(skipReadings < 0)
+                {
+                    total.push_back(totalWeight);
+                    send_weight(total);
+                }
+            }
+
+            ready = false;
+        }
+    }
+}
